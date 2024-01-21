249cf5ef7571bcea3d78ec529259c6cc59722342
Hrithika Gowlikar <145744411+Hrithika22@users.noreply.github.com>
2024-01-21 20:32:16 +0000

    basics done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 123431;
+let accountEmail = "hrithika@google.com"
+var accountPassword = "12233"
+accountCity = "Hyd" //bad practice. Always declare variable
+let accountname;
+console.log(accountEmail,accountCity, accountname);
+/* prefer not to use var because of issue in block and functional code
+*/
