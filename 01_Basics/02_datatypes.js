c9fcb8f3bac261d5ebd22061b93bb00974945eb2
Hrithika Gowlikar <145744411+Hrithika22@users.noreply.github.com>
2024-01-22 20:52:38 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version(new syntax)
+
+//alert(3+3) we are using NodeJs not browser
+console.log(3+1); //code readability should be high tc39.es-ecma script
+console.log("Hrithika")
+let name = "hrithika";//string
+let age = 21; //int
+let isLoggedIn = true;//boolean
+let name;
+// number->2 to the power 52
+// bigint->huge websites
+// string->can use in both single and double quotes. Prefer double quotes
+// boolean->true or false 
+// null->stand alone value(empty). It is an object
+// undefined->value has not been asigned
+// symbol->unique
+
+// object
+console.log(typeof null);
+console.log(typeof undefined);
